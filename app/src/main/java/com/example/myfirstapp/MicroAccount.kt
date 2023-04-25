014c712daa91beb4b10fbb7b0552d3aee44b9641
pawel.markowski <pawel.markowski88@gmail.com>
2023-04-25 18:11:56 +0200

    Add binding for MicroAccount

--- app/src/main/java/com/example/myfirstapp/MicroAccount.kt
+++ app/src/main/java/com/example/myfirstapp/MicroAccount.kt
@@ -5,34 +5,37 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.RadioButton
 import android.widget.Toast
+import com.example.myfirstapp.databinding.ActivityMicroAccountBinding
 import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_micro_account.*
 
 class MicroAccount : AppCompatActivity() {
 
-    var TAG: String = "MicroAccount Activity"
+    private val TAG: String = "MicroAccount Activity"
+    private lateinit var binding: ActivityMicroAccountBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_micro_account)
+        binding = ActivityMicroAccountBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Log.i(TAG,"Open MicroAccount Activity")
 
 
-        CheckInputValue().maxCharacters(edit_text_input_PESEL_or_NIP, message_PESEL_or_NIP, 11,TAG)
+        CheckInputValue().maxCharacters(binding.editTextInputPESELOrNIP, binding.messagePESELOrNIP, 11,TAG)
 
 
-        ReactionOnChecked().changeHintOnCheckedRadioButton(edit_text_input_PESEL_or_NIP,
+        ReactionOnChecked().changeHintOnCheckedRadioButton(binding.editTextInputPESELOrNIP,
             radio_group_PESEL_NIP, "PESEL", this)
 
 
-        Micro_account_generator_button.setOnClickListener{
+        binding.MicroAccountGeneratorButton.setOnClickListener{
 
             Log.i(TAG,"Click on Micro_account_generator_button")
 
-            var PESEL_or_NIP = edit_text_input_PESEL_or_NIP.text.toString()
+            var PESEL_or_NIP = binding.editTextInputPESELOrNIP.text.toString()
 
-            CheckInputValue().checkEmptyInput(PESEL_or_NIP, message_PESEL_or_NIP,
+            CheckInputValue().checkEmptyInput(PESEL_or_NIP, binding.messagePESELOrNIP,
                 "edit_text_input_PESEL_or_NIP",TAG)
 
         }
